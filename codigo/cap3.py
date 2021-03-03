882b4e28e5c319cc28d11773cd8fd1f53d3ca746
VictorMardones <v.mardones04@ufromail.cl>
2021-03-02 22:57:07 -0300

    Empezar capítulo 3

---
+++ codigo/cap3.py
@@ -0,0 +1,24 @@
+
+### string
+print('texto')
+print("texto")
+
+### strings no validos
+# Strings no válidos
+"""
+print('texto")
+print("texto')
+"""
+
+### cadena vacia
+cadena_vacia1 = ''
+cadena_vacia2 = ""
+
+### caracteres especiales
+print('\'')  # '
+print('\"')  # "
+print('\\')  # \
+
+### comillas
+print("This isn't spanish")
+print('Hola "mundo"')
